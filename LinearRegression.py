de46f7398247150c4e84e326a24ad5864e6d5b56
Liviu Vladutu <liviu.vladutu@gmail.com>
2023-05-14 15:27:15 +0300

    Update LinearRegression.py
    Modified the last line to effectively print the regression equation, like in the paper.

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,8 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 # Load the CSV file
-df = pd.read_csv(r'data/data_for_ANN_Fitting.csv')
-
+df = pd.read_csv(r'data/data.csv')
 
 # Get the features and target variables
 X = df[['M', 'logR', 'PGA', 'Ia', 'CAV']].values
@@ -13,6 +12,6 @@ y = df['MMI'].values
 model = LinearRegression()
 model.fit(X, y)
 
-# Print the coefficients
-print(model.coef_)
-print(model.intercept_)
+# Print the equation
+print(f"MMI = {model.intercept_:.3f} + {model.coef_[0]:.3f}M - {model.coef_[1]:.3f}logR + {model.coef_[2]:.3f}PGA + {model.coef_[3]:.3f}Ia - {model.coef_[4]:.3f}CAV")
+
